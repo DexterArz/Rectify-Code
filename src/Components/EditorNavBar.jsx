989018c48fe38aafa53f18f615d59f337302e2e8
akash revankar <dexterArz>
2025-04-04 17:39:59 +0530

    added menue bar for ediitor-bar

--- src/Components/EditorNavBar.jsx
+++ src/Components/EditorNavBar.jsx
@@ -4,7 +4,14 @@ const EditorNavBar = () => {
   return (
     <div id="editorBar">
       <div id="bar">
-        <h3>Reactify-Code</h3>
+      <div id="navPart1">
+      <svg id='menu'onClick={()=>{
+        console.log('menue');
+        
+      }}  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path></svg>
+      <h3>Reactify-Code</h3>
+      </div>
+
         <div id="navPart2">
           <div id="btn">
             <button>Language</button>
