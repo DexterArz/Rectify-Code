88b286f3b350801a5c61f60ebec486f9d8178e41
ADITYA_DR <raybandarkaraditya@gmail.com>
2025-05-16 21:54:56 +0530

    signup.css

--- Reactify-code/src/App.jsx
+++ Reactify-code/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Editor from './components/Editor'
 import Userdashboard from './pages/Userdashboard'
-import Signup from './pages/signup'
+import Signup from './pages/Signup'
 import Signin from './pages/Signin'
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
