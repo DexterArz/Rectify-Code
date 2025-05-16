861a108819dfbd54ee40675924c56fbc397e8e0f
ADITYA_DR <raybandarkaraditya@gmail.com>
2025-05-16 23:00:32 +0530

    cors

--- beckend/src/app.js
+++ beckend/src/app.js
@@ -3,12 +3,16 @@ import cors from 'cors'
 import cookieParser from "cookie-parser"
 
 const app = express()
-app.use(
-    cors({
-        origin : process.env.FRONTEND_URL,
-        credentials : true
-    })
-)
+const corsOptions = {
+  origin: ['https://reactify-code.netlify.app'], // Correct frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Include credentials if needed
+};
+
+app.use(cors(corsOptions));
+
+
 
 app.use(express.json())
 app.use(express.urlencoded())
